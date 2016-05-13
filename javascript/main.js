b25c6e6bede32bd0b319877a3066c1a326dc26e8
Lamar <lcimafra@ucsd.edu>
2016-05-13 01:18:19 -0700

    testing

--- javascript/main.js
+++ javascript/main.js
@@ -49,6 +49,13 @@ function loadHeightPage(dir){
 	});
 	swapScreens(dir);
 }
+function loadWeightPage(dir){
+	$(document).ready(function(){
+  			// Load Body Parts Screen
+		$("#next_screen").load("weight.html .loadable");
+	});
+	swapScreens(dir);
+}
 function swapScreens(dir){
 	if (dir == "down"){
 		$("#next_screen").detach().appendTo("#centerpiece");
